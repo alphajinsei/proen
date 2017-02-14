877e259840f1b8bbc1d2f414fa830ee683ecbd57
alphajinsei <peinn17@gmail.com>
2017-02-14 18:07:56 +0900

    kadai02 ok

--- report2/2report.c
+++ report2/2report.c
@@ -17,7 +17,8 @@ void print_operation(int n);
 struct node *create_node(int *pos, char *s);
 void traverse(struct node *p);
 double calculate(struct node *p);
-
+struct node *differentiate_node(struct node *p);
+struct node *copy_node(struct node *p);
 
 double read_number(int *pos,char *s){//posが今の位置、ctは文字数-1
     int ct=0;
@@ -188,14 +189,245 @@ double calculate(struct node *p){
 }
 
 
+struct node *differentiate_node(struct node *p){
+        struct node *point;
+        point=(struct node*)malloc(sizeof(struct node));
+    if(p->operation==1){
+        struct node *a=copy_node(p->left);
+        struct node *b=copy_node(p->right);
+
+        point->operation=1;
+        point->left=differentiate_node(a);
+        point->right=differentiate_node(b);
+
+    }else if(p->operation==2){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->left);
+        struct node *b1=copy_node(p->right);
+        struct node *b2=copy_node(p->right);
+        struct node *point2;
+        struct node *point3;
+        point2=(struct node*)malloc(sizeof(struct node));
+        point3=(struct node*)malloc(sizeof(struct node));
+        
+        point->operation=1;
+        point->left=point2;
+        point->right=point3;
+        
+        point2->operation=2;
+        point2->left=differentiate_node(a1);
+        point2->right=b1;
+        
+        point3->operation=2;
+        point3->left=a2;
+        point3->right=differentiate_node(b2);
+
+    }else if(p->operation==3){
+        struct node *a=copy_node(p->left);
+        struct node *b=copy_node(p->right);
+
+        point->operation=3;
+        point->left=differentiate_node(a);
+        point->right=differentiate_node(b);
+
+    }else if(p->operation==4){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->left);
+        struct node *b1=copy_node(p->right);
+        struct node *b2=copy_node(p->right);
+        struct node *b3=copy_node(p->right);
+        struct node *point2;
+        struct node *point3;
+        struct node *point4;
+        struct node *point5;
+        struct node *point6;
+        point2=(struct node*)malloc(sizeof(struct node));
+        point3=(struct node*)malloc(sizeof(struct node));
+        point4=(struct node*)malloc(sizeof(struct node));
+        point5=(struct node*)malloc(sizeof(struct node));
+        point6=(struct node*)malloc(sizeof(struct node));
+
+        point->operation=4;
+        point->left=point2;
+        point->right=point5;
+
+        point2->operation=3;
+        point2->left=point3;
+        point2->right=point4;
+
+        point3->operation=2;
+        point3->left=differentiate_node(a1);
+        point3->right=b1;
+
+        point4->operation=2;
+        point4->left=a2;
+        point4->right=differentiate_node(b2);
+
+        point5->operation=9;
+        point5->left=b3;
+        point5->right=point6;
+
+        point6->operation=114514;
+        point6->number=2;
+        point6->left=NULL;
+        point6->right=NULL;
+
+    }else if(p->operation==5){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->left);
+        struct node *point2;
+        point2=(struct node*)malloc(sizeof(struct node));
+
+        point->operation=2;
+        point->left=point2;
+        point->right=differentiate_node(a2);
+
+        point2->operation=6;
+        point2->left=a1;
+        point2->right=NULL;
+
+    }else if(p->operation==6){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->left);
+        struct node *point2;
+        struct node *point3;
+        struct node *point4;
+        point2=(struct node*)malloc(sizeof(struct node));
+        point3=(struct node*)malloc(sizeof(struct node));
+        point4=(struct node*)malloc(sizeof(struct node));
+        point->operation=2;
+        point->left=point2;
+        point->right=differentiate_node(a2);
+
+        point2->operation=2;
+        point2->left=point3;
+        point2->right=point4;
+
+        point3->operation=114514;
+        point3->number=-1;
+        point3->left=NULL;
+        point3->right=NULL;
+
+        point4->operation=5;
+        point4->left=a1;
+        point4->right=NULL;
+
+    }else if(p->operation==7){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->left);
+        struct node *point2;
+        point2=(struct node*)malloc(sizeof(struct node));
+
+        point->operation=2;
+        point->left=point2;
+        point->right=differentiate_node(a2);
+
+        point2->operation=7;
+        point2->left=a1;
+        point2->right=NULL;
+
+    }else if(p->operation==8){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->right);
+
+        point->operation=4;
+        point->left=differentiate_node(a1);
+        point->right=a2;
+
+    }else if(p->operation==9){
+        struct node *a1=copy_node(p->left);
+        struct node *a2=copy_node(p->left);
+        struct node *a3=copy_node(p->left);
+        struct node *a4=copy_node(p->left);
+        struct node *b1=copy_node(p->right);
+        struct node *b2=copy_node(p->right);
+        struct node *b3=copy_node(p->right);
+        struct node *point2;
+        struct node *point3;
+        struct node *point4;
+        struct node *point5;
+        struct node *point6;
+        struct node *point7;
+        point2=(struct node*)malloc(sizeof(struct node));
+        point3=(struct node*)malloc(sizeof(struct node));
+        point4=(struct node*)malloc(sizeof(struct node));
+        point5=(struct node*)malloc(sizeof(struct node));
+        point6=(struct node*)malloc(sizeof(struct node));
+        point7=(struct node*)malloc(sizeof(struct node));
+
+        point->operation=2;
+        point->left=point2;
+        point->right=point3;
+
+        point2->operation=9;
+        point2->left=a1;
+        point2->right=b1;
+
+        point3->operation=1;
+        point3->left=point4;
+        point3->right=point6;
+
+        point4->operation=2;
+        point4->left=differentiate_node(b2);
+        point4->right=point5;
+        
+        point5->operation=8;
+        point5->left=a2;
+        point5->right=NULL;
+
+        point6->operation=2;
+        point6->left=b3;
+        point6->right=point7;
+
+        point7->operation=4;
+        point7->left=differentiate_node(a3);
+        point7->right=a4;
+
+    }else if(p->operation==10){
+        point->operation=114514;
+        point->number=1;
+        point->left=NULL;
+        point->right=NULL;
+    }else{
+        point->operation=114514;
+        point->number=0;
+        point->left=NULL;
+        point->right=NULL;
+    }
+    return point;
+}
+
+struct node *copy_node(struct node *p){
+    struct node *point;
+    point=(struct node*)malloc(sizeof(struct node));
+    point->number=p->number;
+    point->operation=p->operation;
+    if(p->left==NULL){
+        point->left=NULL;
+    }else{
+        point->left=copy_node(p->left);
+    }
+    if(p->right==NULL){
+        point->right=NULL;
+    }else{
+        point->right=copy_node(p->right);
+    }
+}
+
+
 
 int main() {
     //char *str="Plus[Power[x,3],Sin[x]]";
-    char *str="Plus[Times[Sin[13.4],3],2]";
+    //char *str="Plus[Times[Sin[13.4],3],2]";
+    //char *str="Exp[Cos[x]]";
+    char *str="Power[x,x]";
 
     int p = 0;
     struct node *root = create_node(&p, str);
+    struct node *df=differentiate_node(root);
+    printf("D[");
     traverse(root);
-    printf(" = ");
-    printf("%f\n",calculate(root));
+    printf("] = ");
+    traverse(df);
+    //printf("%f\n",calculate(root));
 }
