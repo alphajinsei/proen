64f58ff94a1916e2c3c1578249a1f225052b2f83
alphajinsei <peinn17@gmail.com>
2017-02-16 11:14:56 +0900

    kadai3 ok

--- report2/2report.c
+++ report2/2report.c
@@ -21,11 +21,15 @@ double calculate_function(struct node *p,double x);
 struct node *differentiate_node(struct node *p);
 struct node *copy_node(struct node *p);
 struct node *substitute(struct node *p,double x);
-void differentiate_function(struct node *root);
+double search(double x,struct node *df,struct node *ddf);
+void number_of_x(struct node *p,int *n);
+int iffunc(struct node *p);
+struct node *optimise_node(struct node *p);
+
 
 double read_number(int *pos,char *s){//posが今の位置、ctは文字数-1
     int ct=0;
-    while(('0'<=s[(*pos)+ct]&&s[(*pos)+ct]<='9')||s[(*pos)+ct]=='.'||s[(*pos)+ct]=='x'){
+    while(('0'<=s[(*pos)+ct]&&s[(*pos)+ct]<='9')||s[(*pos)+ct]=='.'||s[(*pos)+ct]=='x'||s[(*pos)+ct]=='-'){
         ct++;
     }
     ct--;
@@ -121,9 +125,9 @@ void print_operation(int n){
 struct node *create_node(int *pos, char *s) {
     struct node *point;
     point=(struct node*)malloc(sizeof(struct node));
-    if('0'<=s[*pos]&&s[*pos]<='9'){
+    if('0'<=s[*pos]&&s[*pos]<='9'||s[*pos]=='-'){
         point->number=read_number(pos,s);
-        point->operation=114514;
+        point->operation=0;
         point->left=NULL;
         point->right=NULL;
     }else if(s[*pos]=='x'){
@@ -132,7 +136,7 @@ struct node *create_node(int *pos, char *s) {
         point->left=NULL;
         point->right=NULL;
     }else{
-        point->number=114514;
+        point->number=0;
         point->operation=read_operation(pos,s);
         point->left=create_node(pos,s);
         if(5<=(point->operation)&&(point->operation)<=8) point->right=NULL;
@@ -153,7 +157,7 @@ void traverse(struct node *p){
     }else if((p->operation)==10){
         printf("x");
     }else{
-        printf("%f",p->number);
+        printf("%.1f",p->number);
     }
 }
 
@@ -270,7 +274,7 @@ struct node *differentiate_node(struct node *p){
         point5->left=b3;
         point5->right=point6;
 
-        point6->operation=114514;
+        point6->operation=0;
         point6->number=2;
         point6->left=NULL;
         point6->right=NULL;
@@ -306,7 +310,7 @@ struct node *differentiate_node(struct node *p){
         point2->left=point3;
         point2->right=point4;
 
-        point3->operation=114514;
+        point3->operation=0;
         point3->number=-1;
         point3->left=NULL;
         point3->right=NULL;
@@ -331,13 +335,68 @@ struct node *differentiate_node(struct node *p){
 
     }else if(p->operation==8){
         struct node *a1=copy_node(p->left);
-        struct node *a2=copy_node(p->right);
+        struct node *a2=copy_node(p->left);
 
         point->operation=4;
         point->left=differentiate_node(a1);
         point->right=a2;
 
     }else if(p->operation==9){
+        if(p->right->operation==0&&p->right->number==0){
+            struct node *a1=copy_node(p->left);
+            struct node *point2;
+            point2=(struct node*)malloc(sizeof(struct node));
+
+            point->operation=2;
+            point->number=0;
+            point->left=differentiate_node(a1);
+            point->right=point2;
+
+            point2->operation=0;
+            point->number=1;
+            point->left=0;
+            point->right=0;
+
+        }else if(p->right->operation==0&&p->right->number!=0){
+            struct node *a1=copy_node(p->left);
+            struct node *a2=copy_node(p->left);
+            struct node *point2;
+            struct node *point3;
+            struct node *point4;
+            struct node *point5;
+            point2=(struct node*)malloc(sizeof(struct node));
+            point3=(struct node*)malloc(sizeof(struct node));
+            point4=(struct node*)malloc(sizeof(struct node));
+            point5=(struct node*)malloc(sizeof(struct node));
+            double n=p->right->number;
+
+            point->operation=2;
+            point->number=0;
+            point->left=point2;
+            point->right=differentiate_node(a2);
+
+            point2->operation=2;
+            point2->number=0;
+            point2->left=point3;
+            point2->right=point4;
+
+            point3->operation=0;
+            point3->number=n;
+            point3->left=NULL;
+            point3->right=NULL;
+
+            point4->operation=9;
+            point4->number=0;
+            point4->left=a1;
+            point4->right=point5;
+
+            point5->operation=0;
+            point5->number=n-1;
+            point5->left=NULL;
+            point5->right=NULL;
+            
+        }else{
+
         struct node *a1=copy_node(p->left);
         struct node *a2=copy_node(p->left);
         struct node *a3=copy_node(p->left);
@@ -385,14 +444,15 @@ struct node *differentiate_node(struct node *p){
         point7->operation=4;
         point7->left=differentiate_node(a3);
         point7->right=a4;
+        }
 
     }else if(p->operation==10){
-        point->operation=114514;
+        point->operation=0;
         point->number=1;
         point->left=NULL;
         point->right=NULL;
     }else{
-        point->operation=114514;
+        point->operation=0;
         point->number=0;
         point->left=NULL;
         point->right=NULL;
@@ -426,52 +486,119 @@ double calculate_function(struct node *p,double x)
 struct node *substitute(struct node *p,double x){
     struct node *point;
     point=(struct node*)malloc(sizeof(struct node));
-    if(point->operation!=10){
+    if(p->operation!=10){
         point->number=p->number;
         point->operation=p->operation;
-    }else{
+    }else if(p->operation==10){
         point->number=x;
-        point->operation=114514;
+        point->operation=0;
     }
     if(p->left==NULL){
         point->left=NULL;
     }else{
-        point->left=copy_node(p->left);
+        point->left=substitute(p->left,x);
     }
     if(p->right==NULL){
         point->right=NULL;
     }else{
-        point->right=copy_node(p->right);
+        point->right=substitute(p->right,x);
     }
 }
 
 
-void differentiate_function(struct node *root){
-    struct node *df=differentiate_node(root);
-    printf("D[");
-    traverse(root);
-    printf("] = ");
-    traverse(df);
-    printf("\n");
+double search(double x,struct node *df,struct node *ddf){
+    double df_x=calculate_function(df,x);
+    double ddf_x=calculate_function(ddf,x);
+    if(fabs(df_x/ddf_x)<0.00001){
+        return x;
+    }else{
+        return search(x-(df_x/ddf_x),df,ddf);
+    }
+}
+
+
+void number_of_x(struct node *p,int *n){
+    if(p->operation==10){
+        (*n)++;
+    }else{
+        if(p->left!=NULL) number_of_x(p->left,n);
+        if(p->right!=NULL) number_of_x(p->right,n);
+    }
+}
+
+int iffunc(struct node *p){//iffunc==0 -> number
+    int n=0;
+    number_of_x(p,&n);
+    int number=n;
+    return number;
+}
+
+struct node *optimise_node(struct node *p){
+    struct node *point;
+    point=(struct node*)malloc(sizeof(struct node));
+    if(iffunc(p)==0){
+        point->number=calculate_number(copy_node(p));
+        point->operation=0;
+        point->left=NULL;
+        point->right=NULL;   
+    }else if(p->operation==2&&p->left->operation==0&&p->left->number==0){
+        point->number=0;
+        point->operation=0;
+        point->left=NULL;
+        point->right=NULL;
+    }else{
+        point->number=p->number;
+        point->operation=p->operation;
+        if(p->left==NULL){
+            point->left=NULL;
+        }else{
+            point->left=optimise_node(p->left);
+        }
+        if(p->right==NULL){
+            point->right=NULL;
+        }else{
+            point->right=optimise_node(p->right);
+        }
+    }
 
 }
 
-int main() {
-    //char *str="Plus[Power[x,3],Sin[x]]";
+int main(int argc,char* argv[]) {
+    //char *str="Plus[Times[x,3],Times[x,-1]]";
     //char *str="Plus[Times[Sin[13.4],3],2]";
-    char *str="Exp[Cos[x]]";
+    //char *str="Exp[Cos[Log[Sin[x]]]]";
     //char *str="Power[x,x]";
+    char *str="Plus[Exp[Times[-1,x]],Power[x,2]]";
+    //char *str="Plus[Log[Plus[1,Power[x,2]]],Power[Plus[x,1],2]]";
+    //char *str="x";
 
     int p=0;
     struct node *f=create_node(&p,str);
-    struct node *df=differentiate_node(f);
-    struct node *ddf=differentiate_node(df);
+    struct node *df=optimise_node(optimise_node(differentiate_node(f)));
+    struct node *ddf=optimise_node(optimise_node(differentiate_node(df)));
 
+    printf("f(x) = ");
+    traverse(f);
+    printf("\n");
     
-    differentiate_function(f);
-    differentiate_function(df);
+    printf("f'(x) = ");
+    traverse(df);
+    printf("\n");
 
-    printf("%f\n",calculate_function(f,0));
-    printf("%f\n",calculate_function(df,0));
-    printf("%f\n",calculate_function(ddf,0));
+    printf("f''(x) = ");
+    traverse(ddf);
+    printf("\n");
+
+    double x0=-3;
+    double solution=search(x0,df,ddf);
+    printf("f'(x)=0 -> x = %f\tf'' = %f\n",solution,calculate_function(ddf,solution));
+    if(calculate_function(ddf,solution)<0){
+        printf("f|max = f(%.5f) = %.5f\n",solution,calculate_function(f,solution));
+    }else{
+        printf("f|min = f(%.5f) = %.5f\n",solution,calculate_function(f,solution));
+
+    }
+
+
+    return 0;
 }
